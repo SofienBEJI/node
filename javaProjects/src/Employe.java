859885454c624abb88fda3db8b370494f5b7a94e
SofienBEJI <Sofien.beji@gmail.com>
2018-09-18 13:54:46 +0100

    Version 1.0

---
+++ javaProjects/src/Employe.java
@@ -0,0 +1,28 @@
+
+public class Employe {
+	
+	private String cin;
+	private String nomPrenom;
+	private String adresse;
+	
+	public String getCin() {
+		return cin;
+	}
+	public void setCin(String cin) {
+		this.cin = cin;
+	}
+	public String getNomPrenom() {
+		return nomPrenom;
+	}
+	public void setNomPrenom(String nomPrenom) {
+		this.nomPrenom = nomPrenom;
+	}
+	public String getAdresse() {
+		return adresse;
+	}
+	public void setAdresse(String adresse) {
+		this.adresse = adresse;
+	}
+	
+
+}
