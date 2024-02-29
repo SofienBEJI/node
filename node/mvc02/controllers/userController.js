6c68e0f977fd9ab5865267f6fdfedd6368a0fef2
sofien.beji <sofien.beji@gmail.com>
2024-02-29 15:29:16 +0100

    node version

---
+++ node/mvc02/controllers/userController.js
@@ -0,0 +1,3 @@
+const User = require('../models/User')
+
+const user = await User.find
---
+++ node/mvc02/index.js
@@ -0,0 +1,9 @@
+const express = require ("express")
+const app = new express();
+
+
+app.listen(5500, () => {
+    console.log('MVC app listening on port 5500!');
+  });
+
+  
